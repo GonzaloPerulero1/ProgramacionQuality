9f97baa7f411a41a8f0c3cfba9c26f28feafae45
Gonzalo Perulero <perulerogonzalo99@gmail.com>
2024-05-06 17:22:13 -0300

    Primer commit

--- obj/Debug/net8.0/ConsoleApp122222.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp122222.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp122222")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78fe4080172905af02617689c8794e4bb776184e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp122222")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp122222")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
